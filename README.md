1a6e54b08c2ea1b44b75e9c67b59016c9e239ab3
IOLevi <337@holbertonschool.com>
2019-03-08 22:03:34 -0800

    working keypad page; punts to Main

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
+IOLevi Index Page
 This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
 
 ## Available Scripts
--- src/index.js
+++ src/index.js
@@ -1,12 +1,123 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+function Square(props) {
+  return (
+    <button className="square" onClick={props.onClick}>
+      {props.value}
+    </button>
+  );
+}
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+class Board extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      squares: Array(9).fill(null),
+    };
+  }
+
+  handleClick(i) {
+    const squares = this.state.squares.slice();
+    if (solvePuzzle(squares) || squares[i]) {
+      return;
+    }
+    
+    if (i === 3 || i === 5) {
+      squares.fill(null)
+    }
+    else {
+      squares[i] = 'X' ;
+    }
+    this.setState({
+      squares: squares,
+    });
+  }
+
+  renderSquare(i) {
+    return (
+      <Square
+        value={this.state.squares[i]}
+        onClick={() => this.handleClick(i)}
+      />
+    );
+  }
+
+  render() {
+    const winner = solvePuzzle(this.state.squares);
+    if (winner) {
+      return (
+        <MainPage />
+      );
+    }
+
+    return (
+      <div>
+        <div className="board-row">
+          {this.renderSquare(0)}
+          {this.renderSquare(1)}
+          {this.renderSquare(2)}
+        </div>
+        <div className="board-row">
+          {this.renderSquare(3)}
+          {this.renderSquare(4)}
+          {this.renderSquare(5)}
+        </div>
+        <div className="board-row">
+          {this.renderSquare(6)}
+          {this.renderSquare(7)}
+          {this.renderSquare(8)}
+        </div>
+      </div>
+    );
+  }
+}
+
+class Game extends React.Component {
+  render() {
+    return (
+      <div className="game">
+        <div className="game-board">
+          <Board />
+        </div>
+        <div className="game-info">
+          <div>{/* status */}</div>
+          <ol>{/* TODO */}</ol>
+        </div>
+      </div>
+    );
+  }
+}
+
+class MainPage extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      test: 1,
+      blah: 2,
+    };
+  }
+
+  render() {
+    return (<div>You did it!</div>)
+  }
+}
+
+// ========================================
+
+ReactDOM.render(
+  <Game />,
+  document.getElementById('root')
+);
+
+// change to be an E will activate something
+function solvePuzzle(squares) {
+  const lines = ['X', 'X', 'X', null, 'X', null, 'X', 'X', 'X'];
+  for (let i = 0; i < lines.length; i++) {
+    if (squares[i] !== lines[i]){
+      return null;
+    }
+  }
+  return true;
+}
